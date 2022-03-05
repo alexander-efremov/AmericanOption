b45f43636d76e9855ce573f3c8f8d7e4b84e8ebc
Aleksander Efremov <aedampir@gmail.com>
2022-03-05 13:39:27 +0700

    wip

--- AmericanOptionAlbena/Program.cs
+++ AmericanOptionAlbena/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
@@ -30,7 +29,7 @@ namespace AmericanOptionAlbena
             const double sigma2 = sigma * sigma; // the squared sigma
             const double r = 0.1d; // the risk-free rate
             const double K = 10; // the strike price
-            const double q = 0d; // the dividend rate
+            const double q = 0.01d; // the dividend rate
             const int N = 800; // the number of space intervals
             const int N1 = N + 1; // the number of points
             const double h = (rb - lb) / N; // the space step
@@ -45,7 +44,17 @@ namespace AmericanOptionAlbena
             // var sols = new List<double[]>();
             var print = true;
 
-            for (var j = 1; j <= M; j++)
+            // s0_hat_deriv[0] = s0_wave[0] = -1; // failed on condition var alpha = r - q - (sigma2 / 2d) + mu_j_l; Debug.Assert(alpha >= 0, "alpha>=0");
+            int j = 0;
+            // s0_hat_deriv[j] = s0_wave[j] = -sigma2 / 2d;
+            // s0_hat_deriv[j] = -eps;
+            s0_hat_deriv[j] = -1d;
+            s0_wave[j] = 0d;
+            var b = (-2d * r) / sigma2;
+            a[j] = (2d * r - 2d * q - sigma2 + 2d * s0_hat_deriv[j]) /
+                   sigma2; // here s0_hat_deriv[j] == mu_j[l] where l = 0
+            lambda[j] = Math.Sqrt(a[j] * a[j] - 4d * b);
+            for (j = 1; j <= M; j++)
             {
                 var mu_j = new double[l_max_iterations];
                 var eta_j = new double[l_max_iterations];
@@ -65,16 +74,15 @@ namespace AmericanOptionAlbena
 
                 while (l < l_max_iterations)
                 {
-                    a[j] = (2d * r - 2d * q - sigma2 + 2d * mu_j[l]) / sigma2;
-                    var b = (-2d * r) / sigma2;
-                    lambda[j] = Math.Sqrt(a[j] * a[j] - 4d * b);
                     Console.WriteLine("l = {0} j = {1} s0_wave[j] = {2}", l, j, rho_j_l);
-                    u_curr = Solve(j, N1, r, q, tau, h, u_prev, sigma2, rho_j_l, s0_wave, mu_j[l], a[j], lambda);
-                    //sols.Add(u_curr);
+                    a[j] = (2d * r - 2d * q - sigma2 + 2d * mu_j[l]) / sigma2;
+                    lambda[j] = Math.Sqrt(a[j] * a[j] - 4d * b);
                     rho_j_l = s0_wave[j - 1] + tau * mu_j[l];
+                    u_curr = Solve(j, N1, K, r, q, tau, h, u_prev, sigma2, rho_j_l, s0_hat_deriv, mu_j[l], a, lambda);
+                    //sols.Add(u_curr);
 
-                    eta_j[l] = K - rho_j_l - u_curr[0];
-                    //eta_j[l] = K * (1d - Math.Exp(rho_j_l)) - u_curr[0];
+                    //eta_j[l] = K - rho_j_l - u_curr[0];
+                    eta_j[l] = K * (1d - Math.Exp(rho_j_l)) - u_curr[0];
                     if (Math.Abs(eta_j[l]) <= Tol)
                     {
                         s0_hat_deriv[j] = mu_j[l];
@@ -84,7 +92,7 @@ namespace AmericanOptionAlbena
 
                     if (++l == 1)
                     {
-                        mu_j[l] = mu_j[0] + eps;
+                        mu_j[l] = mu_j[l - 1] + eps;
                         if (print)
                         {
                             Console.WriteLine("l = {0} eta_j[l-2] = {1}", l, 0d);
@@ -158,34 +166,57 @@ namespace AmericanOptionAlbena
             //         i == 0);
             // }
 
-            for (var i = 2; i < s0_wave.Length; i++)
-                if (s0_wave[i - 1] > s0_wave[i])
-                    throw new InvalidOperationException("The s0 is not convex!");
-
             WriteVectorToFile("S0.dat", s0_wave);
-            PrintToTecplot($"{nameof(s0_wave)}_nx={s0_wave.Length - 1}_tau={tau}.dat", s0_wave, tau);
-            PrintToTecplotT($"{nameof(s0_wave)}_reversed_nx={s0_wave.Length - 1}_tau={tau}.dat", s0_wave, tau, T);
+            WriteVectorToFile("S0_hat_deriv.dat", s0_hat_deriv);
+            // PrintToTecplot($"{nameof(s0_wave)}_nx={s0_wave.Length - 1}_tau={tau}.dat", s0_wave, tau);
+            // PrintToTecplotT($"{nameof(s0_wave)}_reversed_nx={s0_wave.Length - 1}_tau={tau}.dat", s0_wave, tau, T);
 
+            // значения S0 после обратного преобразования
+            PrintS0WaveDirectTime($"{nameof(s0_wave)}_T-t_nx={s0_wave.Length}_tau={tau}.dat", s0_wave, tau, T);
             // print s0 to tecplot file
             var s0 = new double[s0_wave.Length];
             for (var i = 0; i < s0_wave.Length; i++)
                 s0[i] = K * Math.Exp(s0_wave[i]);
-            // PrintToTecplotT($"{nameof(s0)}_T-t_nx={s0.Length}_tau={tau}.dat", s0, tau, T);
-            // PrintToTecplotT($"{nameof(s0)}_T-t_rev_nx={s0.Length}_tau={tau}.dat", s0.Reverse().ToArray(), tau, T);
-            // PrintToTecplot($"{nameof(s0)}_t_nx={s0.Length}_tau={tau}.dat", s0, tau);
-            // PrintToTecplot($"{nameof(s0)}_t_rev_nx={s0.Length}_tau={tau}.dat", s0.Reverse().ToArray(), tau);
+            // значения S0 после обратного преобразования
+            PrintS0DirectTime($"{nameof(s0)}_T-t_nx={s0.Length}_tau={tau}.dat", s0, tau, T);
+            // значения производных
+            PrintS0HatDirectTime($"{nameof(s0_hat_deriv)}_T-t_nx={s0.Length}_tau={tau}.dat", s0_hat_deriv, tau, T);
         }
 
-        private static void PrintToTecplot(string name, double[] s0, double tau)
+        private static void PrintS0DirectTime(string name, double[] arr, double tau, double T)
         {
             using var writer2 = new StreamWriter(name, false);
-            writer2.WriteLine(
-                "TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA'\nVARIABLES = S_0 {0}\nZONE T='{1}'",
-                "t_j", "SubZone");
-            writer2.WriteLine($"I={s0.Length} K={1} ZONETYPE=Ordered");
+            writer2.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
+            writer2.WriteLine("VARIABLES = t S0");
+            writer2.WriteLine("ZONE T='SubZone'");
+            writer2.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
             writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
-            for (var i = 0; i < s0.Length; i++)
-                writer2.WriteLine("{0:e8} {1:e8}", s0[i], tau * i);
+            for (var i = 0; i < arr.Length; i++)
+                writer2.WriteLine("{0:e12} {1:e12}", tau * i, arr[i]);
+        }
+
+        private static void PrintS0WaveDirectTime(string name, double[] arr, double tau, double T)
+        {
+            using var writer2 = new StreamWriter(name, false);
+            writer2.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
+            writer2.WriteLine("VARIABLES = t S0W");
+            writer2.WriteLine("ZONE T='SubZone'");
+            writer2.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
+            for (var i = 0; i < arr.Length; i++)
+                writer2.WriteLine("{0:e12} {1:e12}", tau * i, arr[i]);
+        }
+
+        private static void PrintS0HatDirectTime(string name, double[] arr, double tau, double T)
+        {
+            using var writer2 = new StreamWriter(name, false);
+            writer2.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
+            writer2.WriteLine("VARIABLES = t S0_Deriv");
+            writer2.WriteLine("ZONE T='SubZone'");
+            writer2.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
+            for (var i = 0; i < arr.Length; i++)
+                writer2.WriteLine("{0:e12} {1:e12}", tau * i, arr[i]);
         }
 
         private static void PrintToTecplotT(string name, double[] s0, double tau, double T)
@@ -197,38 +228,58 @@ namespace AmericanOptionAlbena
             writer2.WriteLine($"I={s0.Length - 1} K={1} ZONETYPE=Ordered");
             writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
             for (var i = 0; i < s0.Length; i++)
-                writer2.WriteLine("{0:e8} {1:e8}", s0[i], T - tau * i);
+                writer2.WriteLine("{0:e12} {1:e12}", s0[i], T - tau * i);
         }
 
         private static double[] Solve(
             int j,
             int N1,
+            double K,
             double r,
             double q,
             double tau,
             double h,
             double[] u_prev,
             double sigma2,
-            double rho_j_l, // to toke l-th approximation of s_dash_derivative (t_j)
-            double[] s0_wave, // to toke l-th approximation of s_dash_derivative (t_j)
+            double rho_j_l,
+            double[] s0_hat_deriv,
             double mu_j_l,
-            double a_j,
+            double[] arr_a,
             double[] lambda)
         {
             // calculate the right part
-            var alpha = r - q - (sigma2 / 2d) + mu_j_l;
-            Debug.Assert(alpha >= 0, "alpha>=0");
-            var gamma1 = (1d / tau) - (alpha / h);
-            var gamma2 = (alpha / h);
+            // var alpha = r - q - (sigma2 / 2d) + s0_wave[j - 1];
             var f = new double[N1];
-            f[0] = gamma1 * u_prev[0] + gamma2 * u_prev[1] - (sigma2 * rho_j_l) / h;
-            for (var i = 1; i < N1 - 1; i++)
-                f[i] = gamma1 * u_prev[i] + gamma2 * u_prev[i + 1];
-            var nu_j = 2d / (lambda[j - 1] + lambda[j] + (2d * (s0_wave[j - 1] - mu_j_l)) / sigma2);
-            var u_prev_n_plus_1 = 0d; // u_prev[N1]; TODO: How to calculate the value at the index N1
-            f[N1 - 1] = 0.5d * (gamma1 * u_prev[N1 - 1] 
-                             + gamma2 * u_prev_n_plus_1) 
-                             + (nu_j * u_prev[N1 - 1]) / (tau * h);
+            var alpha_jm1 = r - q - (sigma2 / 2d) + s0_hat_deriv[j - 1];
+            if (alpha_jm1 >= 0d)
+            {
+                var gamma1_jm1 = (1d / tau) - (alpha_jm1 / h);
+                var gamma2_jm1 = (alpha_jm1 / h);
+                f[0] = gamma1_jm1 * u_prev[0] + gamma2_jm1 * u_prev[1] - (sigma2 * K * Math.Exp(rho_j_l)) / h;
+                for (var i = 1; i < N1 - 1; i++)
+                    f[i] = gamma1_jm1 * u_prev[i] + gamma2_jm1 * u_prev[i + 1];
+                var nu_j = 2d / (lambda[j - 1] + lambda[j] + (2d * (s0_hat_deriv[j - 1] - mu_j_l)) / sigma2);
+
+                // formula 31 is applied to j-1 time level
+                // here /*x_{n+1}-x_n= x_n + h - x_n = h*/
+                var u_prev_np1 = u_prev[N1 - 1] * Math.Exp(-((lambda[j - 1] + arr_a[j - 1]) * h) / 2d);
+                f[N1 - 1] = 0.5d * (gamma1_jm1 * u_prev[N1 - 1]
+                                    + gamma2_jm1 * u_prev_np1) + (nu_j * u_prev[N1 - 1]) / (tau * h);
+            }
+            else
+            {
+                var beta1_jm1 = (1d / tau) + (alpha_jm1 / h);
+                var beta2_jm1 = (alpha_jm1 / h);
+                f[0] = (1d / tau) * u_prev[0] - (alpha_jm1 + sigma2 / h) * K * Math.Exp(rho_j_l);
+                for (var i = 1; i < N1 - 1; i++)
+                    f[i] = beta1_jm1 * u_prev[i] - beta2_jm1 * u_prev[i - 1];
+                var nu_j = 2d / (lambda[j - 1] + lambda[j] + (2d * (s0_hat_deriv[j - 1] - mu_j_l)) / sigma2);
+
+                // formula 31 is applied to j-1 time level
+                // here /*x_{n+1}-x_n= x_n + h - x_n = h*/
+                f[N1 - 1] = 0.5d * (beta1_jm1 * u_prev[N1 - 1]
+                                    - beta2_jm1 * u_prev[N1 - 2]) + (nu_j / (tau * h)) * u_prev[N1 - 1];
+            }
 
             // if (l == 1)
             //     Console.WriteLine();
@@ -238,38 +289,32 @@ namespace AmericanOptionAlbena
             // Console.WriteLine(s);
             // Utils.Print(f, "f");
 
-            // b - below main diagonal (indexed as [1;n-1])
-            var a = new double[N1];
-            a[0] = 0d;
-            for (var i = 1; i < N1 - 1; ++i)
-                a[i] = -sigma2 / (2d * h * h);
-
-            a[N1 - 1] = -sigma2 / (2d * h * h); // right boundary condition
+            // a - below main diagonal (indexed as [1;n-1])
+            var a0 = new double[N1];
+            a0[0] = 0d;
+            for (var i = 1; i < N1 - 1; ++i) a0[i] = -sigma2 / (2d * h * h);
+            a0[N1 - 1] = -sigma2 / (2d * h * h);
 
             // main diagonal of matrix (indexed as [0;n-1])
-            var b = new double[N1];
-            b[0] = sigma2 / (h * h) + r + (1d / tau) + (-sigma2 / (2d * h * h));
-            for (var i = 1; i < N1 - 1; ++i)
-                b[i] = sigma2 / (h * h) + r + (1d / tau);
-
+            var b0 = new double[N1];
+            b0[0] = sigma2 / (h * h) + r + (1d / tau);
+            for (var i = 1; i < N1 - 1; ++i) b0[i] = sigma2 / (h * h) + r + (1d / tau);
             var val1 = sigma2 / (2d * h * h);
-            var val2 = sigma2 * (a_j + lambda[j - 1]) / (4d * h);
+            var val2 = (sigma2 * (arr_a[j] + lambda[j])) / (4d * h);
             var val3 = 0.5d * ((1d / tau) + r);
-            var val4 = 1d / (lambda[j - 1] * tau * h);
-            b[N1 - 1] = val1 + val2 + val3 + val4; // right boundary condition
+            var val4 = 1d / (lambda[j] * tau * h);
+            b0[N1 - 1] = val1 + val2 + val3 + val4;
 
-            // d - up to main diagonal (indexed as [0;n-2])
-            var c = new double[N1];
-            c[0] = -sigma2 / (h * h);
-            for (var i = 1; i < N1 - 2; ++i)
-                c[i] = -sigma2 / (2d * h * h);
+            // c - up to main diagonal (indexed as [0;n-2])
+            var c0 = new double[N1];
+            c0[0] = -sigma2 / (h * h);
+            for (var i = 1; i < N1 - 2; ++i) c0[i] = -sigma2 / (2d * h * h);
+            c0[N1 - 2] = 0d;
 
-            // right boundary condition
-            c[N1 - 2] = 0d;
             // Utils.Print(db, "db");
             // Utils.Print(dc, "dc");
             // Utils.Print(dd, "dd");
-            var u = SolveByTridiagonalMatrixAlgorithm(N1, a, b, c, f);
+            var u = SolveByTridiagonalMatrixAlgorithm(N1, a0, b0, c0, f);
             // Utils.Print(u_curr, "u_curr");
             return u;
         }
