4ab7eeaf964036983729e2b04e162984a29e96a8
Aleksander Efremov <aedampir@gmail.com>
2022-05-28 17:45:49 +0700

    wip

--- AmericanOptionAlbena/Program.cs
+++ AmericanOptionAlbena/Program.cs
@@ -7,28 +7,33 @@ using System.Text;
 #pragma warning disable 219
 namespace AmericanOptionAlbena
 {
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+    [SuppressMessage("ReSharper", "CommentTypo")]
+    [SuppressMessage("ReSharper", "ParameterHidesMember")]
     public static class Program
     {
+        private const int l_max_iterations = 1000; // max iteration on l
+        private const double Tol = 10e-10; // eps to refine eta
+        private const double eps = 10e-3; // eps to refine mu
+        private const double lb = 0d; // left bound
+        private const double rb = 100d; // right bound
+        private const double T0 = 0d; // the start time
+        private const double Tn = 60d; // the finish time
+        private const double T = Tn - T0; // time interval
+        private const double sigma = 0.1d; // the sigma = volatility
+        private const double sigma2 = sigma * sigma; // the squared sigma
+        private const double r = 0.1d; // the risk-free rate
+        private const double K = 10; // the strike price
+        private const double q = 0.01d; // the dividend rate
+        private const int N = 800; // the number of space intervals
+        private const int N1 = N + 1; // the number of points
+        private const double h = (rb - lb) / N; // the space step
+        private const int M = 10000; // the number of time intervals
+        private const double tau = T / M; // the time step
+
         public static void Main()
         {
-            const int l_max_iterations = 1000; // max iteration on l
-            const double Tol = 10e-10; // eps to refine eta
-            const double eps = 10e-3; // eps to refine mu
-            const double lb = 0d; // left bound
-            const double rb = 100d; // right bound
-            const double T0 = 0d; // the start time
-            const double Tn = 60d; // the finish time
-            const double T = Tn - T0; // time interval
-            const double sigma = 0.1d; // the sigma = volatility
-            const double sigma2 = sigma * sigma; // the squared sigma
-            const double r = 0.1d; // the risk-free rate
-            const double K = 10; // the strike price
-            const double q = 0.2d; // the dividend rate
-            const int N = 800; // the number of space intervals
-            const int N1 = N + 1; // the number of points
-            const double h = (rb - lb) / N; // the space step
-            const int M = 10000; // the number of time intervals
-            const double tau = T / M; // the time step
             var u_curr = new double[N1]; // the current solution
             var u_prev = new double[N1]; // the previous solution
             var s0_hat_deriv = new double[M + 1];
@@ -41,8 +46,9 @@ namespace AmericanOptionAlbena
             // s0_hat_deriv[0] = s0_wave[0] = -1; // failed on condition var alpha = r - q - (sigma2 / 2d) + mu_j_l; Debug.Assert(alpha >= 0, "alpha>=0");
             int j = 0;
             // s0_hat_deriv[j] = s0_wave[j] = -sigma2 / 2d;
-            // s0_hat_deriv[j] = -eps;
-            s0_hat_deriv[j] = -1d;
+            s0_hat_deriv[j] = -eps;
+            // s0_hat_deriv[j] = -1d;
+            // s0_hat_deriv[j] = -0.5d;
             s0_wave[j] = 0d;
             var b = (-2d * r) / sigma2;
             a[j] = (2d * r - 2d * q - sigma2 + 2d * s0_hat_deriv[j]) /
@@ -52,19 +58,26 @@ namespace AmericanOptionAlbena
             {
                 var mu_j = new double[l_max_iterations];
                 var eta_j = new double[l_max_iterations];
-                var rho_j_l = 0d;
                 var l = 0;
                 mu_j[l] = s0_hat_deriv[j - 1];
 
                 while (l < l_max_iterations)
                 {
-                    Console.WriteLine("l = {0} j = {1} s0_wave[j] = {2}", l, j, rho_j_l);
                     a[j] = (2d * r - 2d * q - sigma2 + 2d * mu_j[l]) / sigma2;
                     lambda[j] = Math.Sqrt(a[j] * a[j] - 4d * b);
-                    rho_j_l = s0_wave[j - 1] + tau * mu_j[l];
-                    u_curr = Solve(j, N1, K, r, q, tau, h, u_prev, sigma2, rho_j_l, s0_hat_deriv, mu_j[l], a, lambda);
+                    var rho_j_l = s0_wave[j - 1] + tau * mu_j[l];
+                    if (double.IsNaN(rho_j_l))
+                    {
+                        Console.WriteLine("rho is NaN!");
+                        break;
+                    }
 
-                    eta_j[l] = K * (1d - Math.Exp(rho_j_l)) - u_curr[0];
+                    u_curr = Solve(j, u_prev, rho_j_l, s0_hat_deriv, mu_j[l], a, lambda);
+
+                    // eta_j[l] = K * (1d - Math.Exp(rho_j_l)) - u_curr[0];
+                    eta_j[l] = K * Math.Exp(rho_j_l) + ((u_curr[1] - u_curr[0]) / h);
+                    Console.WriteLine("l = {0} j = {1} mu_j_l {7} K {2} rho_j_l {3} u[1] {4} u[0] {5}  eta_j_l = {6}",
+                        l, j, K, rho_j_l, u_curr[1], u_curr[0], eta_j[l], mu_j[l]);
                     if (Math.Abs(eta_j[l]) <= Tol)
                     {
                         s0_hat_deriv[j] = mu_j[l];
@@ -73,16 +86,14 @@ namespace AmericanOptionAlbena
                     }
 
                     if (++l == 1)
-                    {
-                        mu_j[l] = mu_j[l - 1] + eps;
-                    }
+                        mu_j[l] = mu_j[l - 1] - eps;
                     else
-                    {
-                        var value = (eta_j[l - 1] * mu_j[l - 2] - eta_j[l - 2] * mu_j[l - 1]) /
-                                    (eta_j[l - 1] - eta_j[l - 2]);
-                        mu_j[l] = value;
-                    }
+                        mu_j[l] = (eta_j[l - 1] * mu_j[l - 2] - eta_j[l - 2] * mu_j[l - 1]) /
+                                  (eta_j[l - 1] - eta_j[l - 2]);
                 }
+
+                WriteVectorToFile("eta_j1_l.arr", eta_j);
+                Console.WriteLine("===========");
 
                 if (l >= l_max_iterations)
                     throw new InvalidOperationException("The 'l' exceeded the maximum number of iterations.");
@@ -105,42 +116,25 @@ namespace AmericanOptionAlbena
             PrintS0ReversedTime($"{nameof(s0)}_T-t_nx={s0.Length}_tau={tau}.dat", s0, tau, T);
         }
 
-        private static double[] Solve(int j, int N1, double K, double r, double q, double tau, double h, double[] u_prev, 
-            double sigma2, double rho_j_l, double[] s0_hat_deriv, double mu_j_l, double[] arr_a, double[] lambda)
+        private static double[] Solve(int j, double[] u_prev, double rho_j_l, double[] s0_hat_deriv, double mu_j_l,
+            double[] arr_a, double[] lambda)
         {
             // calculate the right part
             // var alpha = r - q - (sigma2 / 2d) + s0_wave[j - 1];
             var f = new double[N1];
             var alpha_jm1 = r - q - (sigma2 / 2d) + s0_hat_deriv[j - 1];
-            if (alpha_jm1 >= 0d)
-            {
-                var gamma1_jm1 = (1d / tau) - (alpha_jm1 / h);
-                var gamma2_jm1 = (alpha_jm1 / h);
-                f[0] = gamma1_jm1 * u_prev[0] + gamma2_jm1 * u_prev[1] - (sigma2 * K * Math.Exp(rho_j_l)) / h;
-                for (var i = 1; i < N1 - 1; i++)
-                    f[i] = gamma1_jm1 * u_prev[i] + gamma2_jm1 * u_prev[i + 1];
-                var nu_j = 2d / (lambda[j - 1] + lambda[j] + (2d * (s0_hat_deriv[j - 1] - mu_j_l)) / sigma2);
+            var gamma1_jm1 = (1d / tau) - (alpha_jm1 / h);
+            var gamma2_jm1 = (alpha_jm1 / h);
+            f[0] = K * (1d - Math.Exp(rho_j_l));
+            for (var i = 1; i < N1 - 1; i++)
+                f[i] = gamma1_jm1 * u_prev[i] + gamma2_jm1 * u_prev[i + 1];
+            var nu_j = 2d / (lambda[j - 1] + lambda[j] + (2d * (s0_hat_deriv[j - 1] - mu_j_l)) / sigma2);
 
-                // formula 31 is applied to j-1 time level
-                // here /*x_{n+1}-x_n= x_n + h - x_n = h*/
-                var u_prev_np1 = u_prev[N1 - 1] * Math.Exp(-((lambda[j - 1] + arr_a[j - 1]) * h) / 2d);
-                f[N1 - 1] = 0.5d * (gamma1_jm1 * u_prev[N1 - 1]
-                                    + gamma2_jm1 * u_prev_np1) + (nu_j * u_prev[N1 - 1]) / (tau * h);
-            }
-            else
-            {
-                var beta1_jm1 = (1d / tau) + (alpha_jm1 / h);
-                var beta2_jm1 = (alpha_jm1 / h);
-                f[0] = (1d / tau) * u_prev[0] - (alpha_jm1 + sigma2 / h) * K * Math.Exp(rho_j_l);
-                for (var i = 1; i < N1 - 1; i++)
-                    f[i] = beta1_jm1 * u_prev[i] - beta2_jm1 * u_prev[i - 1];
-                var nu_j = 2d / (lambda[j - 1] + lambda[j] + (2d * (s0_hat_deriv[j - 1] - mu_j_l)) / sigma2);
-
-                // formula 31 is applied to j-1 time level
-                // here /*x_{n+1}-x_n= x_n + h - x_n = h*/
-                f[N1 - 1] = 0.5d * (beta1_jm1 * u_prev[N1 - 1]
-                                    - beta2_jm1 * u_prev[N1 - 2]) + (nu_j / (tau * h)) * u_prev[N1 - 1];
-            }
+            // formula 31 is applied to j-1 time level
+            // here /*x_{n+1}-x_n= x_n + h - x_n = h*/
+            var u_prev_np1 = u_prev[N1 - 1] * Math.Exp(-((lambda[j - 1] + arr_a[j - 1]) * h) / 2d);
+            f[N1 - 1] = 0.5d * (gamma1_jm1 * u_prev[N1 - 1] + gamma2_jm1 * u_prev_np1) +
+                        (nu_j * u_prev[N1 - 1]) / (tau * h);
 
             // if (l == 1)
             //     Console.WriteLine();
@@ -158,7 +152,8 @@ namespace AmericanOptionAlbena
 
             // main diagonal of matrix (indexed as [0;n-1])
             var b0 = new double[N1];
-            b0[0] = sigma2 / (h * h) + r + (1d / tau);
+            // b0[0] = sigma2 / (h * h) + r + (1d / tau);
+            b0[0] = 1d;
             for (var i = 1; i < N1 - 1; ++i) b0[i] = sigma2 / (h * h) + r + (1d / tau);
             var val1 = sigma2 / (2d * h * h);
             var val2 = (sigma2 * (arr_a[j] + lambda[j])) / (4d * h);
@@ -168,7 +163,8 @@ namespace AmericanOptionAlbena
 
             // c - up to main diagonal (indexed as [0;n-2])
             var c0 = new double[N1];
-            c0[0] = -sigma2 / (h * h);
+            // c0[0] = -sigma2 / (h * h);
+            c0[0] = 0d;
             for (var i = 1; i < N1 - 2; ++i) c0[i] = -sigma2 / (2d * h * h);
             c0[N1 - 2] = 0d;
 
@@ -232,58 +228,58 @@ namespace AmericanOptionAlbena
 
         private static void PrintS0ReversedTime(string name, double[] arr, double tau, double T)
         {
-            using var writer2 = new StreamWriter(name, false);
-            writer2.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
-            writer2.WriteLine("VARIABLES = t S0");
-            writer2.WriteLine("ZONE T='SubZone'");
-            writer2.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
-            writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
+            using var writer = new StreamWriter(name!, false);
+            writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
+            writer.WriteLine("VARIABLES = t S0");
+            writer.WriteLine("ZONE T='SubZone'");
+            writer.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
             for (var i = 0; i < arr.Length; i++)
-                writer2.WriteLine("{0:e12} {1:e12}", T - tau * i, arr[i]);
+                writer.WriteLine("{0:e12} {1:e12}", T - tau * i, arr[i]);
         }
 
         private static void PrintS0WaveDirectTime(string name, double[] arr, double tau)
         {
-            using var writer2 = new StreamWriter(name, false);
-            writer2.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
-            writer2.WriteLine("VARIABLES = t S0W");
-            writer2.WriteLine("ZONE T='SubZone'");
-            writer2.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
-            writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
+            using var writer = new StreamWriter(name!, false);
+            writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
+            writer.WriteLine("VARIABLES = t S0W");
+            writer.WriteLine("ZONE T='SubZone'");
+            writer.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
             for (var i = 0; i < arr.Length; i++)
-                writer2.WriteLine("{0:e12} {1:e12}", tau * i, arr[i]);
+                writer.WriteLine("{0:e12} {1:e12}", tau * i, arr[i]);
         }
 
         private static void PrintS0HatDirectTime(string name, double[] arr, double tau)
         {
-            using var writer2 = new StreamWriter(name, false);
-            writer2.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
-            writer2.WriteLine("VARIABLES = t S0_Deriv");
-            writer2.WriteLine("ZONE T='SubZone'");
-            writer2.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
-            writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
+            using var writer = new StreamWriter(name!, false);
+            writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
+            writer.WriteLine("VARIABLES = t S0_Deriv");
+            writer.WriteLine("ZONE T='SubZone'");
+            writer.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
             for (var i = 0; i < arr.Length; i++)
-                writer2.WriteLine("{0:e12} {1:e12}", tau * i, arr[i]);
+                writer.WriteLine("{0:e12} {1:e12}", tau * i, arr[i]);
         }
 
         private static void PrintToTecplotT(string name, double[] s0, double tau, double T)
         {
-            using var writer2 = new StreamWriter(name, false);
-            writer2.WriteLine(
+            using var writer = new StreamWriter(name!, false);
+            writer.WriteLine(
                 "TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA'\nVARIABLES = S {0}\nZONE T='{1}'",
                 "T - t", "SubZone");
-            writer2.WriteLine($"I={s0.Length - 1} K={1} ZONETYPE=Ordered");
-            writer2.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
+            writer.WriteLine($"I={s0.Length - 1} K={1} ZONETYPE=Ordered");
+            writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
             for (var i = 0; i < s0.Length; i++)
-                writer2.WriteLine("{0:e12} {1:e12}", s0[i], T - tau * i);
+                writer.WriteLine("{0:e12} {1:e12}", s0[i], T - tau * i);
         }
 
-        private static void WriteVectorToFile(string path, IList<double> arr)
+        private static void WriteVectorToFile(string path, IEnumerable<double> arr)
         {
-            var sb = new StringBuilder();
-            foreach (var t in arr)
-                sb.AppendLine(t.ToString("e10"));
-            File.WriteAllText(path, sb.ToString());
+            var builder = new StringBuilder();
+            foreach (var a in arr!)
+                builder.AppendLine(a.ToString("e10"));
+            File.WriteAllText(path, builder.ToString());
         }
     }
 }
