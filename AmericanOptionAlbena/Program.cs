94d4a5535a3028f473b914d1acb7bc11f80c07b4
Aleksander Efremov <aedampir@gmail.com>
2022-07-02 19:46:06 +0700

    w

--- AmericanOptionAlbena/Program.cs
+++ AmericanOptionAlbena/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Text;
-
-#pragma warning disable 219
+﻿#pragma warning disable 219
 #pragma warning disable CS0162
 namespace AmericanOptionAlbena
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Text;
+
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     [SuppressMessage("ReSharper", "CommentTypo")]
@@ -35,8 +35,8 @@ namespace AmericanOptionAlbena
 
         // private const int M = 10000; // the number of time intervals
         // private const double M0 = 166; // the time step until we condense the time mesh
-        private const int M = 1000; // the number of time intervals
-        private const double M0 = 16; // the time step until we condense the time mesh
+        private const int M = 10000; // the number of time intervals
+        private const double M0 = 166; // the time step until we condense the time mesh
         private const double tau = T / M; // the time step
         private const double alpha = 2d; // condense parameter for t
         private const double beta = 2d; // condense parameter for h
@@ -49,10 +49,12 @@ namespace AmericanOptionAlbena
 
         public static void Main()
         {
-            PrintCondensedMeshes();
+            // PrintCondensedMeshesTau();
+            // PrintCondensedMeshesH();
+            // return;
             // PrintCondensedMeshes1();
             // PrintCondensedMeshes2();
-            return;
+            // return;
             CheckParameters();
             var u_curr = new double[N1]; // the current solution
             var u_prev = new double[N1]; // the previous solution
@@ -81,7 +83,7 @@ namespace AmericanOptionAlbena
                     lambda[j] = Math.Sqrt(a[j] * a[j] - 4d * b);
                     var tau0 = tau;
                     if (is_condensed_meshes && j < M0)
-                        tau0 = Get_tau1(j, tau, alpha);
+                        tau0 = Get_tau2(j, tau, alpha);
                     var rho_j_l = s0_wave[j - 1] + tau0 * mu_j[l];
                     if (double.IsNaN(rho_j_l))
                     {
@@ -91,12 +93,15 @@ namespace AmericanOptionAlbena
 
                     u_curr = Solve(j, u_prev, rho_j_l, s0_hat_deriv, mu_j[l], a, lambda);
                     // eta_j[l] = K * (1d - Math.Exp(rho_j_l)) - u_curr[0];
+
                     var h0 = h;
                     if (is_condensed_meshes && 1 < N0)
-                        h0 = Get_h1(1, h, beta);
+                        h0 = Get_h2(1, h, beta);
                     eta_j[l] = K * Math.Exp(rho_j_l) + (u_curr[1] - u_curr[0]) / h0;
+
                     Console.WriteLine("l = {0} j = {1} mu_j_l {7} K {2} rho_j_l {3} u[1] {4} u[0] {5}  eta_j_l = {6}",
                         l, j, K, rho_j_l, u_curr[1], u_curr[0], eta_j[l], mu_j[l]);
+
                     if (Math.Abs(eta_j[l]) <= Tol)
                     {
                         s0_hat_deriv[j] = mu_j[l];
@@ -124,13 +129,12 @@ namespace AmericanOptionAlbena
             WriteVectorToFile("S0.arr", s0_wave);
             WriteVectorToFile("S0_hat_deriv.arr", s0_hat_deriv);
             // значения производных
-            PrintS0HatDirectTime($"1_{nameof(s0_hat_deriv)}_t_nx={s0_hat_deriv.Length}_tau={tau}.dat", s0_hat_deriv,
-                tau);
+            PrintS0HatDirectTime($"{(is_condensed_meshes ? "1" : "11")}_{nameof(s0_hat_deriv)}_N1={N1}_T={T}_condensed_{is_condensed_meshes}.dat", s0_hat_deriv, tau, alpha, M0, is_condensed_meshes);
             // значения S0 до обратного преобразования
-            PrintS0WaveDirectTime($"2_{nameof(s0_wave)}_t_nx={s0_wave.Length}_tau={tau}.dat", s0_wave, tau);
+            PrintS0WaveDirectTime($"{(is_condensed_meshes ? "2" : "22")}_{nameof(s0_wave)}_N1={N1}_T={T}_condensed_{is_condensed_meshes}.dat", s0_wave, tau, alpha, M0, is_condensed_meshes);
             var s0 = GetS0(s0_wave);
             // значения S0 после обратного преобразования
-            PrintS0ReversedTime($"3_{nameof(s0)}_T-t_nx={s0.Length}_tau={tau}.dat", s0, tau, T);
+            PrintS0ReversedTime($"{(is_condensed_meshes ? "3" : "33")}_{nameof(s0)}_T-t_N1={N1}_T={T}_condensed_{is_condensed_meshes}.dat", s0, T, tau, alpha, M0, is_condensed_meshes);
         }
 
         private static double[] Solve(int j, double[] u_prev, double rho_j_l, double[] s0_hat_deriv, double mu_j_l,
@@ -146,9 +150,9 @@ namespace AmericanOptionAlbena
                 var h0 = h;
                 var tau0 = tau;
                 if (is_condensed_meshes && i < N0)
-                    h0 = Get_h1(i, h, beta);
+                    h0 = Get_h2(i, h, beta);
                 if (is_condensed_meshes && j < M0)
-                    tau0 = Get_tau1(j, tau, alpha);
+                    tau0 = Get_tau2(j, tau, alpha);
                 var gamma1_jm1_0 = 1d / tau0 - alpha_jm1 / h0;
                 var gamma2_jm1_0 = alpha_jm1 / h0;
                 f[i] = gamma1_jm1_0 * u_prev[i] + gamma2_jm1_0 * u_prev[i + 1];
@@ -171,10 +175,12 @@ namespace AmericanOptionAlbena
             // a - below main diagonal (indexed as [1;n-1])
             a0[0] = 0d;
             for (var i = 1; i < N1 - 1; ++i)
+            {
                 if (is_condensed_meshes && i < N0)
-                    a0[i] = -sigma2 / (Get_h1(i, h, beta) + Get_h1(i + 1, h, beta));
+                    a0[i] = -sigma2 / (Get_h2(i, h, beta) + Get_h2(i + 1, h, beta));
                 else
                     a0[i] = -sigma2 / (2d * h * h);
+            }
 
             a0[N1 - 1] = -sigma2 / (2d * h * h);
 
@@ -182,16 +188,18 @@ namespace AmericanOptionAlbena
             // b0[0] = sigma2 / (h * h) + r + (1d / tau);
             b0[0] = 1d;
             for (var i = 1; i < N1 - 1; ++i)
+            {
                 if (is_condensed_meshes && i < N0)
                 {
-                    var tau0 = j < M0 ? Get_tau1(j, tau, alpha) : tau;
-                    b0[i] = sigma2 / (Get_h1(i, h, beta) * Get_h1(i + 1, h, beta)) + r +
+                    var tau0 = j < M0 ? Get_tau2(j, tau, alpha) : tau;
+                    b0[i] = sigma2 / (Get_h2(i, h, beta) * Get_h2(i + 1, h, beta)) + r +
                             1d / tau0;
                 }
                 else
                 {
                     b0[i] = sigma2 / (h * h) + r + 1d / tau;
                 }
+            }
 
             var val1 = sigma2 / (2d * h * h);
             var val2 = sigma2 * (arr_a[j] + lambda[j]) / (4d * h);
@@ -203,10 +211,12 @@ namespace AmericanOptionAlbena
             // c0[0] = -sigma2 / (h * h);
             c0[0] = 0d;
             for (var i = 1; i < N1 - 2; ++i)
+            {
                 if (is_condensed_meshes && i < N0)
-                    c0[i] = -sigma2 / (Get_h1(i, h, beta) + Get_h1(i + 1, h, beta));
+                    c0[i] = -sigma2 / (Get_h2(i, h, beta) + Get_h2(i + 1, h, beta));
                 else
                     c0[i] = -sigma2 / (2d * h * h);
+            }
 
             c0[N1 - 2] = 0d;
 
@@ -236,10 +246,15 @@ namespace AmericanOptionAlbena
         {
             if (in_i > N0)
                 throw new InvalidOperationException("i > N0!");
-            // начинаем уменьшать шаг поле некоторой точки
+            // начинаем уменьшать шаг после некоторой точки
             var x = in_i * in_h;
-            if (in_i > GetN00())
-                return in_beta * x + (N0 * h - in_beta);
+            if (x > 1d)
+            {
+                var d = in_beta;
+                var e = 1d - in_beta;
+                return d * x + e;
+            }
+
             return Math.Pow(x, in_beta);
         }
 
@@ -247,21 +262,18 @@ namespace AmericanOptionAlbena
         {
             if (in_j > M0)
                 throw new InvalidOperationException("j > M0!");
-            // начинаем уменьшать шаг поле некоторой точки
+
             var t = in_j * in_tau;
-            if (in_j > GetM00())
+            // начинаем уменьшать шаг после некоторой точки M00
+            if (t > 1d)
             {
                 var d = in_alpha;
-                var e = GetM00() * in_tau - in_alpha;
+                var e = 1d - in_alpha;
                 return d * t + e;
             }
 
             return Math.Pow(t, in_alpha);
         }
-
-        private static double GetM00() => M0 - 8;
-
-        private static int GetN00() => N0 / 2;
 
         private static void CheckParameters()
         {
@@ -272,11 +284,12 @@ namespace AmericanOptionAlbena
         }
 
         [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static void PrintCondensedMeshes()
+        private static void PrintCondensedMeshesTau()
         {
             var t = false;
             Console.WriteLine("V1" + new string(' ', 9) + "V2");
             for (var j = 0; j < M0 + 10; j++)
+            {
                 if (j < M0)
                 {
                     var v1 = Get_tau1(j, tau, alpha);
@@ -295,53 +308,87 @@ namespace AmericanOptionAlbena
                     Console.Write((j * tau).ToString("F8") + " " + (j * tau).ToString("F8"));
                     Console.WriteLine();
                 }
+            }
         }
 
         [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static void PrintCondensedMeshes1()
+        private static void PrintCondensedMeshesH()
         {
-            // for (var i = 0; i < N0 + 10; i++)
-            //     if (i < N0)
-            //         Console.Write(Get_h1(i, h, beta) + " ");
-            //     else
-            //         Console.Write(i * h + " ");
-
-            // Console.WriteLine("\r\n=========================\r\n");
-            for (var j = 0; j < M0 + 10; j++)
-                if (j < M0)
-                    Console.Write(Get_tau1(j, tau, alpha).ToString("F8") + " ");
+            var t = false;
+            Console.WriteLine("V1" + new string(' ', 9) + "V2");
+            for (var i = 0; i < N0 + 10; i++)
+            {
+                if (i < N0)
+                {
+                    var v1 = Get_h1(i, h, beta);
+                    var v2 = Get_h2(i, h, beta);
+                    Console.Write(v1.ToString("F8") + " " + v2.ToString("F8") + (v1.Equals(v2) ? '=' : '\0'));
+                    Console.WriteLine();
+                }
                 else
-                    Console.Write((j * tau).ToString("F8") + " ");
+                {
+                    if (!t)
+                    {
+                        Console.WriteLine("constant");
+                        t = true;
+                    }
 
-            Console.WriteLine("\r\n=========================\r\n");
+                    Console.Write((i * h).ToString("F8") + " " + (i * h).ToString("F8"));
+                    Console.WriteLine();
+                }
+            }
         }
 
-        [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static void PrintCondensedMeshes2()
-        {
-            // for (var i = 0; i < N0 + 10; i++)
-            //     if (i < N0)
-            //         Console.Write(Get_h1(i, h, beta) + " ");
-            //     else
-            //         Console.Write(i * h + " ");
+        // [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        // private static void PrintCondensedMeshes1()
+        // {
+        //     // for (var i = 0; i < N0 + 10; i++)
+        //     //     if (i < N0)
+        //     //         Console.Write(Get_h1(i, h, beta) + " ");
+        //     //     else
+        //     //         Console.Write(i * h + " ");
+        //
+        //     // Console.WriteLine("\r\n=========================\r\n");
+        //     for (var j = 0; j < M0 + 10; j++)
+        //     {
+        //         if (j < M0)
+        //             Console.Write(Get_tau1(j, tau, alpha).ToString("F8") + " ");
+        //         else
+        //             Console.Write((j * tau).ToString("F8") + " ");
+        //     }
+        //
+        //     Console.WriteLine("\r\n=========================\r\n");
+        // }
 
-            // Console.WriteLine("\r\n=========================\r\n");
-            for (var j = 0; j < M0 + 10; j++)
-                if (j < M0)
-                    Console.Write(Get_tau2(j, tau, alpha).ToString("G8") + " ");
-                else
-                    Console.Write((j * tau).ToString("G8") + " ");
-        }
-
+        // [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        // private static void PrintCondensedMeshes2()
+        // {
+        //     // for (var i = 0; i < N0 + 10; i++)
+        //     //     if (i < N0)
+        //     //         Console.Write(Get_h1(i, h, beta) + " ");
+        //     //     else
+        //     //         Console.Write(i * h + " ");
+        //
+        //     // Console.WriteLine("\r\n=========================\r\n");
+        //     for (var j = 0; j < M0 + 10; j++)
+        //     {
+        //         if (j < M0)
+        //             Console.Write(Get_tau2(j, tau, alpha).ToString("G8") + " ");
+        //         else
+        //             Console.Write((j * tau).ToString("G8") + " ");
+        //     }
+        // }
 
         private static double[] SolveByTridiagonalMatrixAlgorithm(int n, IReadOnlyList<double> a,
             IReadOnlyList<double> b, IReadOnlyList<double> c, IReadOnlyList<double> d)
         {
             // https://pro-prof.com/forums/topic/sweep-method-for-solving-systems-of-linear-algebraic-equations
             for (var i = 0; i < b.Count; i++)
+            {
                 if (Math.Abs(b[i]) < Math.Abs(a[i]) + Math.Abs(c[i]))
                     throw new Exception(
                         $"There is no diagonal dominance! i={i} {Math.Abs(a[i])} {Math.Abs(b[i])} {Math.Abs(c[i])} sum={Math.Abs(a[i] + c[i])} ");
+            }
             if (Math.Abs(b[0]) < double.Epsilon)
                 throw new InvalidOperationException("c[0] == 0");
             var y = new double[n];
@@ -366,12 +413,12 @@ namespace AmericanOptionAlbena
             return x;
         }
 
-        private static double[] GetS0(double[] s0_wave)
+        private static double[] GetS0(IReadOnlyList<double> s0_wave)
         {
-            var s0 = new double[s0_wave.Length];
-            for (var i = 0; i < s0_wave.Length; i++)
-                s0[i] = K * Math.Exp(s0_wave[i]);
-            return s0;
+            var res = new double[s0_wave.Count];
+            for (var i = 0; i < res.Length; i++)
+                res[i] = K * Math.Exp(s0_wave[i]);
+            return res;
         }
 
         [SuppressMessage("ReSharper", "UnusedMember.Local")]
@@ -394,52 +441,55 @@ namespace AmericanOptionAlbena
             Console.WriteLine("==");
         }
 
-        private static void PrintS0ReversedTime(string name, double[] arr, double tau, double T)
+        private static void PrintS0ReversedTime(string name, IReadOnlyList<double> arr, double T, double in_tau, double in_alpha, double in_M0, bool in_is_condensed_meshes)
         {
             using var writer = new StreamWriter(name!, false);
             writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
             writer.WriteLine("VARIABLES = S0 t");
             writer.WriteLine("ZONE T='SubZone'");
-            writer.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer.WriteLine($"I={arr.Count - 1} K={1} ZONETYPE=Ordered");
             writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
-            for (var i = 0; i < arr.Length; i++)
-                writer.WriteLine("{0:e12} {1:e12}", arr[i], T - tau * i);
+            for (var i = 0; i < arr.Count; i++)
+            {
+                var tau0 = in_tau;
+                if (in_is_condensed_meshes && i < in_M0)
+                    tau0 = Get_tau2(i, in_tau, in_alpha);
+                writer.WriteLine("{0:e16} {1:e16}", arr[i], T - tau0 * i);
+            }
         }
 
-        private static void PrintS0WaveDirectTime(string name, double[] arr, double tau)
+        private static void PrintS0WaveDirectTime(string name, IReadOnlyList<double> arr, double in_tau, double in_alpha, double in_M0, bool in_is_condensed_meshes)
         {
             using var writer = new StreamWriter(name!, false);
             writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
             writer.WriteLine("VARIABLES = S0_W t");
             writer.WriteLine("ZONE T='SubZone'");
-            writer.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer.WriteLine($"I={arr.Count - 1} K={1} ZONETYPE=Ordered");
             writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
-            for (var i = 0; i < arr.Length; i++)
-                writer.WriteLine("{0:e12} {1:e12}", arr[i], tau * i);
+            for (var i = 0; i < arr.Count; i++)
+            {
+                var tau0 = in_tau;
+                if (in_is_condensed_meshes && i < in_M0)
+                    tau0 = Get_tau2(i, in_tau, in_alpha);
+                writer.WriteLine("{0:e16} {1:e16}", arr[i], tau0 * i);
+            }
         }
 
-        private static void PrintS0HatDirectTime(string name, double[] arr, double tau)
+        private static void PrintS0HatDirectTime(string name, IReadOnlyList<double> arr, double in_tau, double in_alpha, double in_M0, bool in_is_condensed_meshes)
         {
             using var writer = new StreamWriter(name!, false);
             writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
             writer.WriteLine("VARIABLES = S0_Deriv t");
             writer.WriteLine("ZONE T='SubZone'");
-            writer.WriteLine($"I={arr.Length - 1} K={1} ZONETYPE=Ordered");
+            writer.WriteLine($"I={arr.Count - 1} K={1} ZONETYPE=Ordered");
             writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
-            for (var i = 0; i < arr.Length; i++)
-                writer.WriteLine("{0:e12} {1:e12}", arr[i], tau * i);
-        }
-
-        [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static void PrintToTecplotT(string name, double[] s0, double tau, double T)
-        {
-            using var writer = new StreamWriter(name!, false);
-            writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA'\nVARIABLES = S {0}\nZONE T='{1}'",
-                "T - t", "SubZone");
-            writer.WriteLine($"I={s0.Length - 1} K={1} ZONETYPE=Ordered");
-            writer.WriteLine("DATAPACKING=POINT\nDT=(DOUBLE DOUBLE)");
-            for (var i = 0; i < s0.Length; i++)
-                writer.WriteLine("{0:e12} {1:e12}", s0[i], T - tau * i);
+            for (var i = 0; i < arr.Count; i++)
+            {
+                var tau0 = in_tau;
+                if (in_is_condensed_meshes && i < in_M0)
+                    tau0 = Get_tau2(i, in_tau, in_alpha);
+                writer.WriteLine("{0:e16} {1:e16}", arr[i], tau0 * i);
+            }
         }
 
         private static void WriteVectorToFile(string path, IEnumerable<double> arr)
