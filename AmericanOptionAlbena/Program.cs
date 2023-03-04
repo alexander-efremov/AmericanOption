f0179482c90f4ecefda76e076cc7f48e59a690fd
Aleksander Efremov <aedampir@gmail.com>
2023-03-04 21:02:18 +0700

    WIP разваливается

--- AmericanOptionAlbena/Program.cs
+++ AmericanOptionAlbena/Program.cs
@@ -44,7 +44,7 @@ namespace AmericanOptionAlbena
         // private const bool is_time_condensed = true; // the parameter which allows enable/disable condensed time mesh
         // private const bool is_space_condensed = true; // the parameter which allows enable/disable condensed space mesh
         private static readonly bool is_condensed_h = false; // the parameter which allows enable/disable condensed meshes
-        private static readonly bool is_condensed_tau = true; // the parameter which allows enable/disable condensed meshes
+        private static readonly bool is_condensed_tau = false; // the parameter which allows enable/disable condensed meshes
 
         private static readonly double[] taus = GetTaus();
         private static readonly double[] hs = GetHs();
@@ -67,7 +67,7 @@ namespace AmericanOptionAlbena
             // s0_hat_deriv[0] = s0_wave[0] = -1; // failed on condition var alpha = r - q - (sigma2 / 2d) + mu_j_l; Debug.Assert(alpha >= 0, "alpha>=0");
             // s0_hat_deriv[j] = s0_wave[j] = -sigma2 / 2d;
             const double b = -2d * r / sigma2;
-            s0_hat_deriv[0] = -0d;
+            s0_hat_deriv[0] = 0d;
             s0_wave[0] = 0d;
             a[0] = (2d * r - 2d * q - sigma2 + 2d * s0_hat_deriv[0]) / sigma2; // here s0_hat_deriv[j] == mu_j[l] where l = 0
             lambda[0] = Math.Sqrt(a[0] * a[0] - 4d * b);
@@ -89,7 +89,11 @@ namespace AmericanOptionAlbena
                     var h0 = is_condensed_h ? hs[0] : h;
                     eta_j[l] = K * Math.Exp(rho_j_l) + (u_curr[1] - u_curr[0]) / h0;
                     if (print)
-                        Console.WriteLine("l = {0} j = {1} mu_j_l {7} K {2} rho_j_l {3} u[1] {4} u[0] {5} eta_j_l = {6}", l, j, K, rho_j_l, u_curr[1], u_curr[0], eta_j[l], mu_j[l]);
+                    {
+                        Console.WriteLine("l = {0:G10} j = {1:G10} K {2:N10} mu_j_l {7:G10} rho_j_l {3:N10} u[1] {4:G10} u[0] {5:G10} eta_j_l = {6:G10}", l, j, K, rho_j_l, u_curr[1], u_curr[0],
+                            eta_j[l], mu_j[l]);
+                    }
+
                     if (Math.Abs(eta_j[l]) <= Tol)
                     {
                         s0_hat_deriv[j] = mu_j[l];
@@ -115,91 +119,76 @@ namespace AmericanOptionAlbena
             S0ReversedTime($"{(is_condensed_h ? "3" : "33")}_{nameof(s0)}_T-t_N1={N1}_T={T}_h_condensed_{is_condensed_h}_tau_condensed_{is_condensed_tau}.dat", s0, T, tau0);
         }
 
-        private static double[] Solve(int j, double[] u_prev, double rho_j_l, double[] s0_hat_deriv, double mu_j_l, double[] arr_a, double[] lambda, double in_tau)
+        private static double[] Solve(int j, double[] u_prev, double rho_j_l, double[] s0_hat_deriv, double mu_j_l, double[] a, double[] lambda, double in_tau)
         {
-            // calculate the right part
-            // var alpha = r - q - (sigma2 / 2d) + s0_wave[j - 1];
+            var alpha_tj = r - q - sigma2 / 2d + s0_hat_deriv[j];
             var f = new double[N1];
-            var alpha_jm1 = r - q - sigma2 / 2d + s0_hat_deriv[j - 1];
-            f[0] = K * (1d - Math.Exp(rho_j_l));
+            var h_12 = is_condensed_h ? hs[0] : h;
+            var h_32 = is_condensed_h ? hs[1] : h;
+            var u_prev_1 = u_prev[1];
+            var u_0j = K * (1d - Math.Exp(rho_j_l));
+            f[0] = u_prev_1 / in_tau + (sigma2 / (h_12 * (h_12 + h_32))) * u_0j;
             for (var i = 1; i < N1 - 1; i++)
-            {
-                var h_jp12 = is_condensed_h ? hs[i] : h;
-                var gamma1_jm1_0 = 1d / in_tau - alpha_jm1 / h_jp12;
-                var gamma2_jm1_0 = alpha_jm1 / h_jp12;
-                f[i] = gamma1_jm1_0 * u_prev[i] + gamma2_jm1_0 * u_prev[i + 1];
-            }
+                f[i] = u_prev[i] / in_tau;
+            var h_Nm12 = is_condensed_h ? hs[N1 - 1] : h;
+            var nu_j = 2d / (lambda[j - 1] + lambda[j] + 2d * (s0_hat_deriv[j - 1] - mu_j_l) / sigma2);
+            f[N1 - 1] = u_prev[N1 - 1] / (2d * in_tau) + nu_j * u_prev[N1 - 1] / (in_tau * h_Nm12);
 
-            // formula 31 is applied to j-1 time level
-            // here /*x_{n+1}-x_n= x_n + h - x_n = h*/
-            {
-                var nu_j = 2d / (lambda[j - 1] + lambda[j] + 2d * (s0_hat_deriv[j - 1] - mu_j_l) / sigma2);
-                var h_jp12 = is_condensed_h ? hs[N1 - 1] : h;
-                var gamma1_jm1 = 1d / in_tau - alpha_jm1 / h_jp12;
-                var gamma2_jm1 = alpha_jm1 / h_jp12;
-                var u_prev_np1 = u_prev[N1 - 1] * Math.Exp(-((lambda[j - 1] + arr_a[j - 1]) * h_jp12) / 2d);
-                f[N1 - 1] = 0.5d * (gamma1_jm1 * u_prev[N1 - 1] + gamma2_jm1 * u_prev_np1) + nu_j * u_prev[N1 - 1] / (in_tau * h_jp12);
-            }
-
-            var a0 = new double[N1];
-            var b0 = new double[N1];
-            var c0 = new double[N1];
-
-            // a - below main diagonal (indexed as [1;n-1])
+            var a0 = new double[N1]; // a - below main diagonal (indexed as [1;n-1])
+            var b0 = new double[N1]; // b - main diagonal of matrix (indexed as [0;n-1])
+            var c0 = new double[N1]; // c - up to main diagonal (indexed as [0;n-2])
             a0[0] = 0d;
             for (var i = 1; i < N1 - 1; ++i)
                 if (is_condensed_h)
-                    a0[i] = -sigma2 / (hs[i - 1] * (hs[i - 1] * hs[i]));
+                    a0[i] = -sigma2 / (hs[i - 1] * (hs[i - 1] + hs[i]));
                 else
                     a0[i] = -sigma2 / (2d * h * h);
 
             if (is_condensed_h)
-                a0[N1 - 1] = -sigma2 / (hs[N1 - 2] * (hs[N1 - 2] + hs[N1 - 1]));
+                a0[N1 - 1] = -sigma2 / (2d * hs[N1 - 1] * hs[N1 - 1]) + alpha_tj / (2d * hs[N1 - 1]);
             else
-                a0[N1 - 1] = -sigma2 / (2d * h * h);
+                a0[N1 - 1] = -sigma2 / (2d * h * h) + alpha_tj / (2d * h);
 
-            // main diagonal of matrix (indexed as [0;n-1])
-            // b0[0] = sigma2 / (h * h) + r + (1d / tau);
-            b0[0] = 1d;
+            if (is_condensed_h)
+                b0[0] = sigma2 / (hs[0] * (hs[0] + hs[1])) + sigma2 / (hs[1] * (hs[0] + hs[1])) + r + 1d / in_tau + alpha_tj / hs[1];
+            else
+                b0[0] = sigma2 / (2d * h * h) + sigma2 / (2d * h * h) + r + 1d / in_tau + alpha_tj / h;
             for (var i = 1; i < N1 - 1; ++i)
                 if (is_condensed_h)
-                    b0[i] = sigma2 / (hs[i - 1] * hs[i]) + r + 1d / in_tau;
+                    b0[i] = sigma2 / (hs[i - 1] * (hs[i - 1] + hs[i])) + sigma2 / (hs[i] * (hs[i - 1] + hs[i])) + r + 1d / in_tau + alpha_tj / hs[i];
                 else
-                    b0[i] = sigma2 / (h * h) + r + 1d / in_tau;
+                    b0[i] = sigma2 / (2d * h * h) + sigma2 / (2d * h * h) + r + 1d / in_tau + alpha_tj / h;
 
             if (is_condensed_h)
             {
-                // what h we use here? hp12 or hm12?
-                var val1 = sigma2 / (hs[N1 - 2] * hs[N1 - 1]);
-                var val2 = sigma2 * (arr_a[j] + lambda[j]) / (4d * hs[N1 - 1]);
-                var val3 = 0.5d * (1d / in_tau + r);
-                var val4 = 1d / (lambda[j] * in_tau * hs[N1 - 1]);
-                b0[N1 - 1] = val1 + val2 + val3 + val4;
+                var val1 = (sigma2 / 2d) * (1d / (hs[N1 - 1] * hs[N1 - 1]) + (a[j] + lambda[j]) / (2d * hs[N1 - 1]));
+                var val2 = 0.5d * (1d / in_tau + r) + 1d / (lambda[j] * in_tau * hs[N1 - 1]);
+                var val3 = alpha_tj / (2d * hs[N1 - 1]);
+                b0[N1 - 1] = val1 + val2 - val3;
             }
             else
             {
-                var val1 = sigma2 / (2d * h * h);
-                var val2 = sigma2 * (arr_a[j] + lambda[j]) / (4d * h);
-                var val3 = 0.5d * (1d / in_tau + r);
-                var val4 = 1d / (lambda[j] * in_tau * h);
-                b0[N1 - 1] = val1 + val2 + val3 + val4;
+                var val1 = (sigma2 / 2d) * (1d / (h * h) + (alpha_tj + lambda[j]) / (2d * h));
+                var val2 = 0.5d * (1d / in_tau + r) + 1d / (lambda[j] * in_tau * h);
+                var val3 = alpha_tj / (2d * h);
+                b0[N1 - 1] = val1 + val2 - val3;
             }
 
-            // c - up to main diagonal (indexed as [0;n-2])
-            // c0[0] = -sigma2 / (h * h);
-            c0[0] = 0d;
+            if (is_condensed_h)
+                c0[0] = -sigma2 / (hs[0] * (hs[0] + hs[1])) - alpha_tj / hs[1];
+            else
+                c0[0] = -sigma2 / (2d * h * h) - alpha_tj / h;
             for (var i = 1; i < N1 - 2; ++i)
                 if (is_condensed_h)
-                    c0[i] = -sigma2 / (hs[i] * (hs[i - 1] + hs[i]));
+                    c0[i] = -sigma2 / (hs[i] * (hs[i - 1] + hs[i])) - alpha_tj / hs[i];
                 else
-                    c0[i] = -sigma2 / (2d * h * h);
-
+                    c0[i] = -sigma2 / (2d * h * h) - alpha_tj / hs[i];
             c0[N1 - 2] = 0d;
 
+            var u = SolveByTridiagonalMatrixAlgorithm(N1, a0, b0, c0, f);
             // Utils.Print(db, "db");
             // Utils.Print(dc, "dc");
             // Utils.Print(dd, "dd");
-            var u = SolveByTridiagonalMatrixAlgorithm(N1, a0, b0, c0, f);
             // Utils.Print(u_curr, "u_curr");
             return u;
         }
@@ -238,40 +227,7 @@ namespace AmericanOptionAlbena
             return Math.Pow(t, in_alpha);
         }
 
-        [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static void PrintCondensedMeshesTau()
-        {
-            Console.WriteLine($"M={M} Tn={Tn} T0={T0} T={T} alpha={alpha} tau0={tau0}");
-            Console.WriteLine("TAU V1" + new string(' ', 10) + "TAU V2");
-            var t = 0d;
-            for (var j = 1; j < 110; j++)
-            {
-                var t1 = gt1(j * tau0, alpha) - gt1((j - 1) * tau0, alpha);
-                var t2 = taus[j - 1];
-                t += t2;
-                Console.Write("j: " + j + " " + t1.ToString("F8") + " " + t2.ToString("F8") + " " + t.ToString("F8") + (t1.Equals(t2) ? '=' : '\0'));
-                Console.WriteLine();
-            }
-        }
-
-        [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static void PrintCondensedMeshesH()
-        {
-            Console.WriteLine($"N={N} beta={beta} h={h}");
-            Console.WriteLine("H V1" + new string(' ', 12) + "H V2");
-            var x = 0d;
-            for (var i = 1; i < 100; i++)
-            {
-                var x1 = gh1(i * h, beta) - gh1((i - 1) * h, beta);
-                var x2 = hs[i - 1];
-                x += x2;
-                Console.Write("i: " + i + " " + x1.ToString("F8") + " " + x2.ToString("F8") + " " + x.ToString("F8") + (x1.Equals(x2) ? '=' : '\0'));
-                Console.WriteLine();
-            }
-        }
-
-        private static double[] SolveByTridiagonalMatrixAlgorithm(int n, IReadOnlyList<double> a,
-            IReadOnlyList<double> b, IReadOnlyList<double> c, IReadOnlyList<double> d)
+        private static double[] SolveByTridiagonalMatrixAlgorithm(int n, IReadOnlyList<double> a, IReadOnlyList<double> b, IReadOnlyList<double> c, IReadOnlyList<double> d)
         {
             for (var i = 0; i < b.Count; i++)
                 if (Math.Abs(b[i]) < Math.Abs(a[i]) + Math.Abs(c[i]))
@@ -386,7 +342,6 @@ namespace AmericanOptionAlbena
             writer.WriteLine("TITLE = 'DEM DATA | DEM DATA | DEM DATA | DEM DATA');");
             writer.WriteLine("VARIABLES = S0 t");
             writer.WriteLine("ZONE T='SubZone'");
-
             var t = T;
             var j = 0;
             var s = string.Empty;
@@ -445,8 +400,42 @@ namespace AmericanOptionAlbena
 
         private static void CheckNaN(double val)
         {
-            if (double.IsNaN(val))
-                throw new Exception("rho is NaN!");
+            if (!double.IsNaN(val))
+                return;
+            Console.Out.Flush();
+            throw new Exception("rho is NaN!");
+        }
+        
+        [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        private static void PrintCondensedMeshesTau()
+        {
+            Console.WriteLine($"M={M} Tn={Tn} T0={T0} T={T} alpha={alpha} tau0={tau0}");
+            Console.WriteLine("TAU V1" + new string(' ', 10) + "TAU V2");
+            var t = 0d;
+            for (var j = 1; j < 110; j++)
+            {
+                var t1 = gt1(j * tau0, alpha) - gt1((j - 1) * tau0, alpha);
+                var t2 = taus[j - 1];
+                t += t2;
+                Console.Write("j: " + j + " " + t1.ToString("F8") + " " + t2.ToString("F8") + " " + t.ToString("F8") + (t1.Equals(t2) ? '=' : '\0'));
+                Console.WriteLine();
+            }
+        }
+
+        [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        private static void PrintCondensedMeshesH()
+        {
+            Console.WriteLine($"N={N} beta={beta} h={h}");
+            Console.WriteLine("H V1" + new string(' ', 12) + "H V2");
+            var x = 0d;
+            for (var i = 1; i < 100; i++)
+            {
+                var x1 = gh1(i * h, beta) - gh1((i - 1) * h, beta);
+                var x2 = hs[i - 1];
+                x += x2;
+                Console.Write("i: " + i + " " + x1.ToString("F8") + " " + x2.ToString("F8") + " " + x.ToString("F8") + (x1.Equals(x2) ? '=' : '\0'));
+                Console.WriteLine();
+            }
         }
     }
 }
